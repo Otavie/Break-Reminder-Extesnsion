ffa571895d762883ddc9585db0c177cf61be1430
Otavie <otavieokuoyo@gmail.com>
2024-08-11 10:43:51 +0100

    Add isBreakReminderEnabled var and code that ensure that Stop Reminder btn stops the code from running at the background

--- background.js
+++ background.js
@@ -3,6 +3,9 @@ const BREAK_ALARM_NAME = "BREAK_ALARM_NAME"
 // Flag to Check if the Alarm is Currently Active
 let isAlarmActive = false
 
+// Flag to Check if the Break Reminder is Enabled
+let isBreakReminderEnabled = false
+
 // Store the Idle State Listener to Remove It Later
 let idleStateListener = null
 
@@ -22,19 +25,22 @@ function stopAlarm() {
 // Function to Start Monitoring Idle Start
 function startIdleStateChecker() {
     idleStateListener = (newState) => {
-        // Start Timer Alarm if User is Active and Alarm is not Already Active
-        if (newState === "active" && !isAlarmActive) {
-            console.log("User is active. Starting the break reminder timer.")
-            chrome.storage.local.get("breakInterval", (result) => {
-                const interval = result.breakInterval
-                createAlarm(interval)
-            })
-            // Stop Timer if User is Idle and the Alarm is Active
-        } else if (newState === "idle" && isAlarmActive) {
-            console.log("User is idle or away. Break reminder timer is stopped!")
-            stopAlarm()
+        if (isBreakReminderEnabled) {
+            // Start Timer Alarm if User is Active and Alarm is not Already Active
+            if (newState === "active" && !isAlarmActive) {
+                console.log("User is active. Starting the break reminder timer.")
+                chrome.storage.local.get("breakInterval", (result) => {
+                    const interval = result.breakInterval
+                    createAlarm(interval)
+                })
+                // Stop Timer if User is Idle and the Alarm is Active
+            } else if (newState === "idle" && isAlarmActive) {
+                console.log("User is idle or away. Break reminder timer is stopped!")
+                stopAlarm()
+            }
         }
     }
+
     chrome.idle.onStateChanged.addListener(idleStateListener)
 }  
 
@@ -51,7 +57,9 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Break Reminder Extension Installed!")
     chrome.storage.local.get("breakInterval", (result) => {
         const interval = result.breakInterval
-        createAlarm(interval)
+        if (isBreakReminderEnabled) {
+            createAlarm(interval)
+        }
         startIdleStateChecker()
     })
 })
@@ -71,11 +79,14 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 chrome.runtime.onMessage.addListener((request) => {
     if (request.event === "startBreakTimer") {
         chrome.storage.local.get("breakInterval", (result) => {
-            createAlarm(result.breakInterval)
+            const interval = result.breakInterval
+            isBreakReminderEnabled = true
+            createAlarm(interval)
             startIdleStateChecker()
         })
     } else if (request.event === "stopBreakTimer") {
         stopAlarm()
+        isBreakReminderEnabled = false
         stopIdleStateChecker()
     }
 })
