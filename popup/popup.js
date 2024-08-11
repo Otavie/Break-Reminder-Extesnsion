d4a7fd7e583d3691de1031b969d720caa44f95b7
Otavie <otavieokuoyo@gmail.com>
2024-08-11 09:21:33 +0100

    Add code for notifying user if they clicked on Start or Stop buttons

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,22 @@ const saveBTN = document.querySelector(".save-btn");
 const stopBTN = document.querySelector(".stop-btn");
 const errorMessage = document.querySelector(".errorMessage");
 const durationInput = document.getElementById("duration-input")
+const startMessage = document.querySelector(".start-message");
+const stopMessage = document.querySelector(".stop-message");
+
+stopBTN.disabled = true
+
+const displayMessage = (messageElt, otherMessageElt) => {
+    if (otherMessageElt) {
+        otherMessageElt.style.display = "none"
+    }
+
+    messageElt.style.display = "flex"
+
+    setTimeout(() => {
+        messageElt.style.display = "none"
+    }, 5000)
+}
 
 const saveAndStart = () => {
     const duration = parseFloat(durationInput.value)
@@ -17,11 +33,19 @@ const saveAndStart = () => {
     chrome.storage.local.set({ breakInterval: duration }, () => {
         console.log(`Break interval set to ${duration} minutes.`)
         chrome.runtime.sendMessage({ event: "startBreakTimer" })
+
+        displayMessage(startMessage, stopMessage)
     })
+
+    // saveBTN.disabled = true
+    stopBTN.disabled = false
 }
 
 const stopReminder = () => {
     chrome.runtime.sendMessage({ event: "stopBreakTimer" })
+    displayMessage(stopMessage, startMessage)
+    saveBTN.disabled = false
+    stopBTN.disabled = true
 }
 
 saveBTN.onclick = saveAndStart
